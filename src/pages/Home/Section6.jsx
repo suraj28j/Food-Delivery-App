2c5a0db26259145090b151ea233def36d348fc60
Suraj Kumar <suraj28j@gmail.com>
2024-12-02 18:36:45 +0530

    home blog section update

--- src/pages/Home/Section6.jsx
+++ src/pages/Home/Section6.jsx
@@ -51,36 +51,65 @@ const Section6 = () => {
                     </div> */}
 
 
-                    <div id="carouselExampleCaptions" class="carousel slide">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
                         </div>
-                        <div class="carousel-inner">
-                            
-                            <div class="carousel-item active">
-                                <div className=''>
+                        <div class="carousel-inner mb-4">
+                            <div class="carousel-item active" data-bs-interval="2500">
+                                <div className='text-center'>
                                     <img src={User1} class="" alt="User1" />
-                                    <div class="carousel-caption ">
-                                        <p>" Etiam sapien sem at sagittis congue augue massa varius
-                                            sodales sapien undo tempus dolor egestas magna suscipit magna
-                                            tempus aliquet porta sodales augue suscipit luctus neque "</p>
-                                        <div className='item_rating mb-3'>
-                                            <i className='bi bi-star-fill'></i>
-                                            <i className='bi bi-star-fill'></i>
-                                            <i className='bi bi-star-fill'></i>
-                                            <i className='bi bi-star-fill'></i>
-                                            <i className='bi bi-star-fill'></i>
-                                        </div>
-                                        <h5>BY AMELIE NEWLOVE</h5>
+                                    <p>" Etiam sapien sem at sagittis congue augue massa varius
+                                        sodales sapien undo tempus dolor egestas magna suscipit magna
+                                        tempus aliquet porta sodales augue suscipit luctus neque "</p>
+                                    <div className='item_rating mb-3'>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
                                     </div>
+                                    <h5>BY AMELIE NEWLOVE</h5>
                                 </div>
                             </div>
-
-                            <div class="carousel-item">
-                                <img src={User2} class="" alt="User2" />
-                                <div class="carousel-caption ">
+                            <div class="carousel-item" data-bs-interval="2500">
+                                <div className='text-center'>
+                                    <img src={User2} class="" alt="User2" />
+                                    <p>" Etiam sapien sem at sagittis congue augue massa varius
+                                        sodales sapien undo tempus dolor egestas magna suscipit magna
+                                        tempus aliquet porta sodales augue suscipit luctus neque "</p>
+                                    <div className='item_rating mb-3'>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                    </div>
+                                    <h5>BY AMELIE NEWLOVE</h5>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2500">
+                                <div className='text-center'>
+                                    <img src={User3} class="" alt="User3" />
+                                    <p>" Etiam sapien sem at sagittis congue augue massa varius
+                                        sodales sapien undo tempus dolor egestas magna suscipit magna
+                                        tempus aliquet porta sodales augue suscipit luctus neque "</p>
+                                    <div className='item_rating mb-3'>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                        <i className='bi bi-star-fill'></i>
+                                    </div>
+                                    <h5>BY AMELIE NEWLOVE</h5>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2500">
+                                <div className='text-center'>
+                                    <img src={User4} class="" alt="User4" />
                                     <p>" Etiam sapien sem at sagittis congue augue massa varius
                                         sodales sapien undo tempus dolor egestas magna suscipit magna
                                         tempus aliquet porta sodales augue suscipit luctus neque "</p>
@@ -95,11 +124,11 @@ const Section6 = () => {
                                 </div>
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
